ef939f9e151f4611e3f2f335b9cc0feddb3c0f95
valu <valentina.sanchez.castrillon@correounivalle.edu.co>
2024-11-21 12:31:54 -0500

    funcion Rho y Rho par

--- src/main/scala/Opinion/package.scala
+++ src/main/scala/Opinion/package.scala
@@ -27,38 +27,105 @@ package object Opinion {
   // rho(sb, d) es la polarizacion de los agentes
   // de acuerdo a esa medida
   def rho(alpha: Double, beta: Double): AgentsPolMeasure = {
-    // rho es la medida de polarizacion de agentes basada
-    // en comete
+    /**
+     * @param specificBelief Vector de creencias (valores entre 0 y 1)
+     * @param distributionValues Valores discretos para la distribución
+     * @return Valor de polarización normalizado entre 0 (mín) y 1 (máx)
+     */
 
+    (specificBelief: SpecificBelief, distributionValues: DistributionValues) => {
+      // Inicializamos el vector de frecuencias con ceros
+      val emptyFrequencies = Vector.fill(distributionValues.length)(0.0)
+      // Calculamos las frecuencias para cada valor de la distribución
+      val rawFrequencies = specificBelief.foldLeft(emptyFrequencies) { (accumulatedFreq, agentBelief) =>
+        // Encontramos el valor más cercano en la distribución para la creencia
+        val closestValueIndex = distributionValues.indices.minBy { index =>
+          math.abs(distributionValues(index) - agentBelief)
+        }
+        // Incrementamos la frecuencia para ese valor
+        accumulatedFreq.updated(closestValueIndex, accumulatedFreq(closestValueIndex) + 1.0)
+      }
+      // Normalizamos las frecuencias dividiendo por el número total de agentes
+      val totalAgents = specificBelief.length
+      val normalizedFrequencies = rawFrequencies.map(frequency => frequency / totalAgents)
+      // Creamos la distribución como un par de (frecuencias normalizadas, valores)
+      val beliefDistribution = (normalizedFrequencies, distributionValues)
+      // Creamos la medida de -comete- con los parámetros alpha y beta
+      val baseCometeMeasure = Comete.rhoCMT_Gen(alpha, beta)
+      // Normalizamos la medida respecto al peor caso(máxima polarización)
+      val normalizedCometeMeasure = Comete.normalizar(baseCometeMeasure)
+      // aqui se calcula y se devuelve la polarización normalizada
+      normalizedCometeMeasure(beliefDistribution)
+    }
   }
+
 
   // Tipos para Modelar la evolucion de la opinion en una red
-  type WeightedGraph = (Int, Int) => Double
-  type SpecificWeightedGraph = (WeightedGraph, Int)
-  type GenericWeightedGraph =
-    Int => SpecificWeightedGraph
-  type FunctionUpdate = (SpecificBelief, SpecificWeightedGraph) => SpecificBelief
+  //type WeightedGraph = (Int, Int) => Double
+  //type SpecificWeightedGraph = (WeightedGraph, Int)
+  //type GenericWeightedGraph =
+    //Int => SpecificWeightedGraph
+    //type FunctionUpdate = (SpecificBelief, SpecificWeightedGraph) => SpecificBelief
 
-  def confBiasUpdate(sb: SpecificBelief, swg: SpecificWeightedGraph): SpecificBelief = {
+ //** def confBiasUpdate(sb: SpecificBelief, swg: SpecificWeightedGraph): SpecificBelief = {
 
-  }
+ // }
 
-  def showWeightedGraph(swg: SpecificWeightedGraph): IndexedSeq[IndexedSeq[Double]] = {
+  //def showWeightedGraph(swg: SpecificWeightedGraph): IndexedSeq[IndexedSeq[Double]] = {
+  //}
 
-  }
+  //def simulate(fu: FunctionUpdate, swg: SpecificWeightedGraph, b0: SpecificBelief, t: Int): IndexedSeq[SpecificBelief] = {
 
-  def simulate(fu: FunctionUpdate, swg: SpecificWeightedGraph, b0: SpecificBelief, t: Int): IndexedSeq[SpecificBelief] = {
-
-  }
+ // }
 
   // Versiones paralelas
   def rhoPar(alpha: Double, beta: Double): AgentsPolMeasure = {
     // rho es la medida de polarizacion de agentes basada
-    // en comete
+    /**
+     * Versión paralela de la medida de polarización basada en Comete.
+     *
+     * @param specificBelief Vector de creencias (valores entre 0 y 1)
+     * @param distributionValues Valores discretos para la distribución
+     * @return Valor de polarización normalizado entre 0 (mín) y 1 (máx)
+     */
+    (specificBelief: SpecificBelief, distributionValues: DistributionValues) => {
+      // Inicializamos el vector de frecuencias con ceros
+      val emptyFrequencies = Vector.fill(distributionValues.length)(0.0)
 
+      // Paralelizamos el cálculo de frecuencias usando ParVector
+      val rawFrequencies = specificBelief.par.aggregate(emptyFrequencies)(
+        // Función combinadora para cada creencia
+        (accumulatedFreq, agentBelief) => {
+          // Encontramos el valor más cercano en la distribución
+          val closestValueIndex = distributionValues.indices.minBy { index =>
+            math.abs(distributionValues(index) - agentBelief)
+          }
+          // Incrementamos la frecuencia para ese valor
+          accumulatedFreq.updated(closestValueIndex, accumulatedFreq(closestValueIndex) + 1.0)
+        },
+        // Combina resultados parciales
+        (freq1, freq2) => freq1.zip(freq2).map { case (a, b) => a + b }
+      )
+
+      // Normalizamos las frecuencias dividiendo por el número total de agentes
+      val totalAgents = specificBelief.length
+      val normalizedFrequencies = rawFrequencies.map(frequency => frequency / totalAgents)
+
+      // Creamos la distribución como un par de (frecuencias normalizadas, valores)
+      val beliefDistribution = (normalizedFrequencies, distributionValues)
+
+      // Creamos la medida de Comete con los parámetros alpha y beta
+      val baseCometeMeasure = Comete.rhoCMT_Gen(alpha, beta)
+
+      // Normalizamos la medida respecto al peor caso (máxima polarización)
+      val normalizedCometeMeasure = Comete.normalizar(baseCometeMeasure)
+
+      // Calcula y devuelve la polarización normalizada
+      normalizedCometeMeasure(beliefDistribution)
+    }
   }
 
-  def confBiasUpdatePar(sb: SpecificBelief, swg: SpecificWeightedGraph): SpecificBelief = {
+ // def confBiasUpdatePar(sb: SpecificBelief, swg: SpecificWeightedGraph): SpecificBelief = {
 
-  }
+  //}
 }
